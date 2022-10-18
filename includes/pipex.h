be41a216a0c3b08ef577d27af5507a264b5bcc6b
unknown <jamie.gworld@hotmail.com>
2022-10-18 23:20:39 +0700

    OK

---
+++ includes/pipex.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbureera <pbureera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 23:01:56 by pbureera          #+#    #+#             */
+/*   Updated: 2022/10/18 23:01:56 by pbureera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include "../libft/libft.h"
+
+/* Mandatory functions */
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+int		get_next_line(char **line);
+void	execute(char *argv, char **envp);
+
+/* Bonus functions */
+int		open_file(char *argv, int i);
+void	usage(void);
+
+#endif
